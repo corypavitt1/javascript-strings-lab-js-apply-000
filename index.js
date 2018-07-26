435a17561cad1dd113e7024cdb398b0dfea90205
Cory Pavitt <corypavitt@hotmail.com>
2018-07-26 01:57:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var greeting = "Hello, everybody!";
 
-var specialGuest = "Neil deGrasse Tyson"
+var specialGuest = "Hello, Neil deGrasse Tyson"
 
 var greetSpecialGuest = "" + specialGuest + "!";
 
-var topic = "space";
+var topic = "let\'s tak about space";
 
 var conversation = `${topic}`;
